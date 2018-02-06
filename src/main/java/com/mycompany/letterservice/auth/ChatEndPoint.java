da55c139c6e34d3db2188d3cd518c120d02d2b63
nekres <shikoryakm@gmail.com>
2018-02-06 15:08:28 +0200

    websockets enabled

--- src/main/java/com/mycompany/letterservice/auth/ChatEndPoint.java
+++ src/main/java/com/mycompany/letterservice/auth/ChatEndPoint.java
@@ -5,9 +5,10 @@
  */
 package com.mycompany.letterservice.auth;
 
+import java.io.IOException;
 import java.util.logging.Logger;
-import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.OnClose;
+import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
@@ -22,15 +23,21 @@ public class ChatEndPoint {
     private final Logger logger = Logger.getLogger(this.getClass().getName());
     @OnOpen
     public void open(Session session){
-        logger.info("OnOpen: " + session.toString());
+        logger.info("OnOpen: " + session.getId());
+        
     }
     @OnClose
     public void close(Session session){
         logger.info("OnClose: " + session.toString());
     }
     @OnMessage
-    public void message(Session session){
+    public void message(String message, Session session) throws IOException{
         logger.info("OnMessage: " + session.toString());
+        
+        session.getBasicRemote().sendText(message);
     }
     
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+    }
 }
--- src/main/webapp/js/WebsocketUtils.js
+++ src/main/webapp/js/WebsocketUtils.js
@@ -4,18 +4,35 @@
  * and open the template in the editor.
  */
 
-
+var socket = null;
 function doConnect(){
-    var socket = new WebSocket('ws://localhost:8084/LetterService/listener');
-    socket.onopen = function(event) {
-        alert();
-    //socketStatus.innerHTML = 'Connected to: ' + event.currentTarget.url;
-    //socketStatus.className = 'open';
-    };
-    socket.setReadyState();
-//    socket.onmessage = function(event) {
-//  alert("Получены данные " + event.data);
-//    };
-//    socket.send("Привет");
+    var uri = 'ws://' + window.location.host + "/letter/listener";
+    alert(uri);
+    
+      if ('WebSocket' in window) {
+                socket = new WebSocket(uri);
+            } else if ('MozWebSocket' in window) {
+                socket = new MozWebSocket(uri);
+            }else{
+                alert("websockets not suppored in ur browser");
+            }
+    socket.onopen = function () {
+                socket.send("jsk");
+            };
+            socket.onmessage = function (event) {
+                alert("Received message:" + event.data);
+            };
+            socket.onclose = function (event) {
+                setConnected(false);
+            };
+            
 }
+function updateTarget(target) {
+            if (window.location.protocol == 'http:') {
+                document.getElementById('target').value = 'ws://' + window.location.host + target;
+            } else {
+                document.getElementById('target').value = 'wss://' + window.location.host + target;
+            }
+        }
+
     
